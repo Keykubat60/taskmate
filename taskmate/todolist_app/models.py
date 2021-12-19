cba5249f9dbb8165ff6f56c7c158254dd902b91b
unknown <alaaddinsagun60@gmail.com>
2021-12-20 00:59:31 +0100

    add model

--- taskmate/todolist_app/models.py
+++ taskmate/todolist_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TaskList(models.Model):
+    task = models.CharField(max_length=300)
+    done = models.BooleanField(default=False)
+
