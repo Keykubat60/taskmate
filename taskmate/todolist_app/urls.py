486deba56d946a85740bff9a734e6543536d151a
unknown <alaaddinsagun60@gmail.com>
2021-12-19 23:40:23 +0100

    add base.html about.html contact.html

--- taskmate/todolist_app/urls.py
+++ taskmate/todolist_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 
 ]
--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,4 +3,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return HttpResponse("HelloWorld")
+    context = {
+
+    }
+    return render(request, 'todolist.html', context)
+
+def contact(request):
+    """
+    1. rendert ein HTML datei als Antwort zurück
+    2. Ein inhalt{} wird immer erstellt auch wenn es leer sein sollte
+
+    """
+    context = {
+        'contact': "Welcome to Contact Page.",
+
+    }
+    return render(request, 'contact.html',context)
+
+
+def about(request):
+    """
+    1. rendert ein HTML datei als Antwort zurück
+    2. Ein inhalt{} wird immer erstellt auch wenn es leer sein sollte
+
+    """
+    context = {
+        'about': "Welcome to About Page.",
+
+    }
+    return render(request, 'about.html',context)
