4e1a9d3b7b667c3e928d681770311216192e1278
unknown <alaaddinsagun60@gmail.com>
2021-12-20 20:56:25 +0100

    setup hompage

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =  [(os.path.join(BASE_DIR,'static'))]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
