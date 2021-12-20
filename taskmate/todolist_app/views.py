efad0830ee7003ffcdc18260160bd2838ed3fb69
unknown <alaaddinsagun60@gmail.com>
2021-12-20 02:04:59 +0100

    add task as completed

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -39,6 +39,20 @@ def edit_task(request,task_id):
 
         return render(request, 'edit.html', {'task_obj': task_obj})
 
+def complete_task(request,task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    return redirect("todolist")
+
+def pending_task(request,task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    return redirect("todolist")
+
+
+
 def contact(request):
     """
     1. rendert ein HTML datei als Antwort zurück
