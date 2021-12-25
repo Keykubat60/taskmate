d4668a5c6a00f6463769f21d9f550a1db77c2fff
unknown <alaaddinsagun60@gmail.com>
2021-12-25 16:44:52 +0100

    add view register

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
 
 def register(request):
-    return HttpResponse("REGISTER PAGE")
+
+    if request.method == "POST":
+        register_form = UserCreationForm(request)
+        #register_form.save()
+        messages.success(request,("SUCCESFUL you create an user account!"))
+        return redirect("register")
+    else:
+
+
+        register_form = UserCreationForm()
+    return render(request, "register.html", {"register_form": register_form})
