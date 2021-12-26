f3beed10da2d400c2973e51f7ab0ecb092447398
Alaaddin Ertugrul Sagun <alaaddinsagun60@gmail.com>
2021-12-26 20:36:21 +0100

    add logout.html

--- taskmate/users_app/urls.py
+++ taskmate/users_app/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('login', auth_views.LoginView.as_view(template_name = 'login.html'), name= 'login'),
-path('logout', auth_views.LogoutView.as_view(), name= 'logout'),
+path('logout', auth_views.LogoutView.as_view(template_name = 'logout.html'), name= 'logout'),
 ]
