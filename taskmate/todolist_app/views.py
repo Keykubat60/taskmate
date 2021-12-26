af3112d2a06fe1afa9ce91a7555356eb605df677
Alaaddin Ertugrul Sagun <alaaddinsagun60@gmail.com>
2021-12-26 21:39:32 +0100

    add login_required to todolist

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -3,9 +3,10 @@ from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+@login_required()
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
